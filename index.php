66d60969f2d21ba27d5c78253a16ab51551975a2
Suketu <ashish.chocha@sparsh1.com>
2018-04-27 14:28:22 +0530

    test form

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<title>Test</title>
 </head>
 <body>
-	<h1>Test with autocomplete off in form itself</h1>
+	<h3>Test with autocomplete off in form itself</h3>
 <form autocomplete="off">
 	<label>City:</label>
 	<input type="text" name="city" ><br><br><br>
@@ -17,12 +17,22 @@
 	<textarea name="address" ></textarea>
 </form>
 	<h1>===========================</h1>
-	<h1>Test with autocomplete off in inidividual inputs</h1>
+	<h3>Test with autocomplete off in inidividual inputs</h3>
 <form autocomplete="">
 	<label>City:</label>
 	<input type="text" name="city" autocomplete="off"><br><br><br>
 	<label>Address:</label>
 	<textarea name="address" autocomplete="off"></textarea>
+</form>
+
+	<h1>===========================</h1>
+	<h3>Tests Solutions</h3>
+<form autocomplete="">
+	<label>City:</label>
+	<input type="text" name="city" autocomplete="" auto-complete="something-new"><br><br><br>
+	<label>Address:</label>
+	<textarea name="address" autocomplete="" value="&nbsp;"></textarea>
+	<input type="submit" name="submit" value="submit">
 </form>
 </body>
 </html>
