ae1efb080363c6ee0f5bc05945d0de5df63711a4
Suketu <ashish.chocha@sparsh1.com>
2018-05-25 09:53:19 +0530

    test

---
+++ test.js
@@ -0,0 +1,35 @@
+	// initialScreenSize = screen.height, //Checks initial height of the screen
+        var initialScreenSize = window.innerHeight; //Checks initial height of the screen
+         $(document).on('click','input',function(){
+            console.log('asd');
+            if(check()){
+                scrollUP();
+            }else{
+                scrollDown();
+            };    
+         }) //Setting interval to check screensize changes / not
+        
+        
+        function check(){ //Actual screen height change checking code
+            // var kbactive = screen.height;
+            var kbactive = window.innerHeight;
+            if(initialScreenSize > kbactive ){
+                scrollUP();
+                return true;
+            }else{
+                scrollDown();
+                return false;    
+            }
+        }
+        
+        function scrollUP(){ //Adds keyboard active class
+            i = $('.user').offset().top;
+            while(i>50){
+                $('.login-modal').scrollTop($('.login-modal').scrollTop()+i);
+                i--;
+            }
+        }
+        
+        function scrollDown(){ //Removes keyboard active class
+            $('.login-modal').scrollTop(0)
+        }
